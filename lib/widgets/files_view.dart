94741512932bd4e6f8d0df2324740baa9b4a4d12
Mareai BinTaleb <102855431+Mar3e@users.noreply.github.com>
2024-01-17 23:26:13 +0300

    filling the space and colors

--- lib/widgets/files_view.dart
+++ lib/widgets/files_view.dart
@@ -1,22 +1,39 @@
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:markdown_editor/utils/colors.dart';
+import 'package:markdown_editor/core/utils/colors.dart';
 
 class FilesView extends StatelessWidget {
   const FilesView({
     super.key,
   });
 
+  List<Widget> get testWidgets {
+    var widgets = Iterable.generate(
+      10,
+      (index) {
+        return ListTile(
+          onPressed: () {},
+          title: const Text('File Name'),
+        );
+      },
+    ).toList();
+
+    return widgets;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
         padding: const EdgeInsets.symmetric(
-          horizontal: 16.0,
+          horizontal: 8.0,
           vertical: 24.0,
         ),
-        color: AppColors.secondaryColor,
+        color: AppColors.secondaryColor.withOpacity(0.9),
         height: double.infinity,
-        child: const Text('text'),
+        child: ListView.builder(
+          itemCount: testWidgets.length,
+          itemBuilder: (context, index) => testWidgets[index],
+        ),
       ),
     );
   }
